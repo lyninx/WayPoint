0bb73067de7b5d42d903cd77e6fa104cb2cc2780
Michael <m.ding5692@gmail.com>
2016-03-05 17:59:39 -0500

    Added markers to maps

--- public/app.js
+++ public/app.js
@@ -27,63 +27,11 @@
 
   app.controller('MapSelectCtrl', function($scope, $http, trip) {
     $scope.trip = trip;
-      initMap();
-      function initMap() {
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 6,
-          center: {lat: 41.85, lng: -87.65}
-        });
-        directionsDisplay.setMap(map);
-        
-        document.getElementById('submit').addEventListener('click', function() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
-        });
-      }
-
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        var waypts = [];
-        var checkboxArray = document.getElementById('waypoints');
-        for (var i = 0; i < checkboxArray.length; i++) {
-         // if (checkboxArray.options[i].selected) {
-            waypts.push({
-              location: checkboxArray[i].value,
-              stopover: true
-            });
-         // }
-        }
-        var selectedMode = document.getElementById('mode').value;
-        directionsService.route({
-          origin: $scope.trip.origin,
-          destination: document.getElementById('end').value,
-          waypoints: waypts,
-          optimizeWaypoints: true,
-          travelMode: google.maps.TravelMode[selectedMode]
-        }, function(response, status) {
-          if (status === google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-            var route = response.routes[0];
-            var summaryPanel = document.getElementById('directions-panel');
-            summaryPanel.innerHTML = '';
-            // For each route, display summary information.
-            for (var i = 0; i < route.legs.length; i++) {
-              var routeSegment = i + 1;
-              summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-                  '</b><br>';
-              summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-              summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-              summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-            }
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
     $scope.newWayPoint = "";
     $scope.mapModel = [];
-    $scope.yelpModel = [];
-    $scope.markerArr = [];
+    var yelpModel = [];
+    var markerArr = [];
+    var icon = "./icons/marker.png";
 
     $scope.addWayPoint = function() {
       $scope.mapModel.push($scope.newWayPoint);
@@ -105,9 +53,92 @@
       console.log("pressed")
       $http.get('http://api.lyninx.com/showRecommendations')
       .then(function(res) {
-        console.log("got a response");
-        console.log(res);
+        
+        for (var j = 0; j < res.data.length; j++) {
+          var resIndex = res.data[j];
+          
+          var wayPtObj = {
+            price: resIndex.priceDescription,
+            category: resIndex.categories[0][0],
+            address: resIndex.location.address[0],
+            lat: resIndex.location.coordinate.latitude,
+            lon: resIndex.location.coordinate.longitude,
+            rating: resIndex.reviewInfo.rating,
+            name: resIndex.name
+          };
+
+          console.log(wayPtObj);
+          yelpModel.push(wayPtObj);
+
+          var marker = new google.maps.Marker({
+            position: {lat:wayPtObj.lat, lng: wayPtObj.lon },
+            title: wayPtObj.name,
+            icon: icon
+          });
+
+          markerArr.push(marker);
+          //console.log(markerArr);
+          //console.log("marker Pushed");
+        }
+
       });
     };
+
+      initMap();
+      function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 6,
+          center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
+        
+        document.getElementById('submit').addEventListener('click', function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        });
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var waypts = [];
+        var checkboxArray = document.getElementById('waypoints');
+        for (var i = 0; i < checkboxArray.length; i++) {
+            waypts.push({
+              location: checkboxArray[i].value,
+              stopover: true
+            });
+        }
+        var selectedMode = document.getElementById('mode').value;
+        directionsService.route({
+          origin: $scope.trip.origin,
+          destination: document.getElementById('end').value,
+          waypoints: waypts,
+          optimizeWaypoints: true,
+          travelMode: google.maps.TravelMode[selectedMode]
+        }, function(response, status) {
+          if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+
+            for (var index = 0; index < markerArr.length;index++) {
+              markerArr[index].setMap(map);
+            }
+
+            var route = response.routes[0];
+            var summaryPanel = document.getElementById('directions-panel');
+            summaryPanel.innerHTML = '';
+            // For each route, display summary information.
+            for (var i = 0; i < route.legs.length; i++) {
+              var routeSegment = i + 1;
+              summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                  '</b><br>';
+              summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+              summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+              summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+            }
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
   });
 })()
