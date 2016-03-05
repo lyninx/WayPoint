44427237fe6ef12f335e24074ca27995a41c0119
leroywan <leroywan@gmail.com>
2016-03-05 03:10:18 -0500

    created search function for foods and hotels

--- yelpSearch.js
+++ yelpSearch.js
@@ -15,18 +15,47 @@ var client = yelp.createClient({
   }
 });
 
+// create constructor for business
+var Business = function(name, reviewInfo, phoneNumber, location, categories, url){
+  this.name = name;
+  this.reviewInfo = reviewInfo;
+  this.phoneNumber = phoneNumber;
+  this.location = location;
+  this.categories = categories;
+  this.url = url;
+}
 
-var findBusinesses = function(terms, postalCode){
+showBusinesses = function(businesses){
+  var businessArray = [];
+  for(var i=0; i<businesses.length; i++){
+    // get business information
+    var name = businesses[i].name;
+    var reviewInfo = {reviewCount: businesses[i].review_count, rating: businesses[i].rating};
+    var phoneNumber = businesses[i].phone;
+    var location = businesses[i].location;
+    var categories = businesses[i].categories;
+    var url = businesses[i].url;
+
+    // create a new object with the search results and push into array
+    var businessObject = new Business(name, reviewInfo, phoneNumber, location, categories, url)
+    businessArray.push(businessObject);
+  }
+  // return info on top 5 search results
+  return businessArray
+}
+
+var findBusinesses = function(terms, categoryFilter ,postalCode, radiusFilter){
   client.search({
     terms: terms,
     location: postalCode,
-    limit: 2,
+    limit: 5,
     sort: 0,
-    radius_filter: 2000
+    radius_filter: radiusFilter,
+    category_filter: categoryFilter
   }).then(function (data) {
     var businesses = data.businesses;
     var location = data.region;
-    console.log(businesses);
+    console.log(showBusinesses(businesses));
   
   }).catch(function (err) {
     if (err.type === yelp.errorTypes.areaTooLarge) {
@@ -37,10 +66,7 @@ var findBusinesses = function(terms, postalCode){
   });
 };
  
-client.business("grand-place-bruxelles-2", {
-  cc: "US"
-}).then(function (data) {
-  // ... 
-});
 
-findBusinesses("Food", "M2M3Z9");
+
+findBusinesses("hotel", "hotels" ,"M2M3Z9", 5000); //returns information on hotels at a postal code with a 5000m radius
+findBusinesses("food", "food", "M2M3Z9", 2000); // returns information on food ...
