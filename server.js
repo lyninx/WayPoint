61f926da12b5d1bd9dda82002ab918d5e576f9e7
Michael <m.ding5692@gmail.com>
2016-03-05 10:39:18 -0500

    Remove redundant code

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ var bodyParser = require('body-parser')
 var cors = require('cors');
 var sassMiddleware = require('node-sass-middleware');
 var morgan = require('morgan');
-var router = require('./router.js');
 var mongoose = require('mongoose');
 var db = require('./model/database.js')
 var app = express();
-var api = express();
-var admin = express();
 
 var port = 3000;
-var api_port = 3001;
+
 
 //////////////////////////////////////////////////////////////////
 mongoose.connect('mongodb://admin:qhacks@ds060968.mlab.com:60968/tripninja', function(err){
@@ -30,12 +27,6 @@ app.use(sassMiddleware({
     outputStyle: 'expanded'
 }));
 
-api.set('port', api_port);
-api.use(bodyParser.json());
-api.use(bodyParser.urlencoded({ extended: false }));
-api.use(cors());
-api.use(router);
-
 app.set('port', process.env.PORT || port);
 app.use(morgan('dev')); // log every request to the console
 app.use(express.static(__dirname + "/public"));
@@ -43,7 +34,16 @@ app.use('/*', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', function(req,res) {
+    res.sendfile("public/index.html");
+});
+
 app.post('/postWayPt', function(req,res) {
+    console.log("here I am");
     var newWayPoint = req.body;
     console.log("database inserts " + newWayPoint);
     db.insertWaypoint(newWayPoint);
@@ -53,6 +53,4 @@ app.post('/postWayPt', function(req,res) {
 app.listen(port, function() {
     console.log('listening on port: '+ port);
 });
-api.listen(api_port, function(){
-	console.log('api on port: '+ api_port);
-});
+
